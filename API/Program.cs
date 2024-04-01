6cfb71ffd5b803315b6d7657c9ee6c3eb74920b1
Norah Lovelace <call1800norah@gmail.com>
2024-04-01 13:55:59 -0500

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,11 @@
+using System.Text;
+using API.Data;
 using API.Extensions;
+using API.Interface;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
